97e3fb30f039a96e95c5c3be26b992e0f3cc5254
Jon Warman <warman@sendjack.com>
2012-10-11 05:37:57 -0400

    [deck] ported all of it outside this repository for safer keeping

--- urls.py
+++ urls.py
@@ -5,12 +5,9 @@ URLs for the Tornado handlers.
 """
 from handlers.signup import SignUpHandler
 from handlers.signup_submit import SignUpSubmitHandler
-from handlers.deck import DeckHandler
 
 
 url_patterns = [
         (r"/", SignUpHandler),
         (r"/signup/submit", SignUpSubmitHandler),
-        (r"/deck/", DeckHandler),
-        (r"/deck", DeckHandler),
         ]
