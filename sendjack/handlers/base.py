8debd63a6c6595b4dfa4c025ff5451c84fab487b
Jon Warman <warman@sendjack.com>
2012-12-31 09:24:15 -0500

    [controller] add the basics for the crud and base handler to work once we hook up a view
    - simple login session so we can limit template editing to just us [if we set a
      cookie from the command line]
    - async/sync request processing
    
    next would be to build a model in the app layer and try to generically pass
    straight through from CRUDHandler without any custom code in the controller.

--- sendjack/handlers/base.py
+++ sendjack/handlers/base.py
@@ -14,7 +14,32 @@ Simple class hierarchy for the controller's cookie functionality so that
 handlers don't have to repeat work and can take advantage of a good API.
 
 """
+import json
 import tornado.web
+
+from util.decorators import constant
+
+
+class _Argument(object):
+
+    @constant
+    def ASYNCHRONOUS(self):
+        return "asynchronous"
+
+    @constant
+    def PARAMETERS(self):
+        return "parameters"
+
+ARGUMENT = _Argument()
+
+
+class _COOKIE(object):
+
+    @constant
+    def SESSION(self):
+        return "session"
+
+COOKIE = _COOKIE()
 
 
 class BaseHandler(tornado.web.RequestHandler):
@@ -33,3 +58,53 @@ class BaseHandler(tornado.web.RequestHandler):
 
         """
         return None
+
+
+    def process_request(self):
+        # TODO: grab errors subclassing this from jackalope and raise those.
+        raise NotImplementedError()
+
+
+    def process_asynchronous_request(self):
+        # TODO: grab errors subclassing this from jackalope and raise those.
+        raise NotImplementedError()
+
+
+    def process_synchronous_request(self):
+        # TODO: grab errors subclassing this from jackalope and raise those.
+        raise NotImplementedError()
+
+
+    def markup_path(self):
+        # TODO: grab errors subclassing this from jackalope and raise those.
+        raise NotImplementedError()
+
+
+    def content_model(self):
+        # TODO: grab errors subclassing this from jackalope and raise those.
+        raise NotImplementedError()
+
+
+    def is_asynchronous_request(self):
+        return bool(self.get_argument(ARGUMENT.ASYNCHRONOUS, False))
+
+
+    def get_request_parameters(self):
+        return json.loads(self.get_argument(ARGUMENT.PARAMETERS, None))
+
+
+    def set_session(self, cookie):
+        self.set_secure_cookie(
+                COOKIE.SESSION,
+                tornado.escape.json_encode(cookie))
+
+
+    def get_session(self):
+        session = self.get_secure_cookie(COOKIE.SESSION)
+        return tornado.escape.json_decode(session) if session else None
+
+
+    def end_session(self):
+        session = self.get_session()
+        self.clear_cookie(COOKIE.SESSION)
+        return session
