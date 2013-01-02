59c8450fb1c0f93ce2255c3f4760a27eba03859d
Jon Warman <warman@sendjack.com>
2013-01-02 07:17:54 -0500

    [urls] alpha.sendjack.com now redirects to https://secure.sendjack.com/signup
    since we dont have anything but our alpha pointed at sendjack for now
    its just fine to assume anything getting through is alpha and to just redirect
    to secure and change that signup endpoint.

--- sendjack/urls.py
+++ sendjack/urls.py
@@ -3,11 +3,14 @@
 URLs for the Tornado handlers.
 
 """
+from tornado.web import RedirectHandler
+
 from handlers.signup import SignUpHandler
 from handlers.signup_submit import SignUpSubmitHandler
 
 
 url_patterns = [
-        (r"/", SignUpHandler),
+        (r"/", RedirectHandler, {"url": "https://secure.sendjack.com/signup"}),
+        (r"/signup", SignUpHandler),
         (r"/thankyou", SignUpSubmitHandler),
         ]
