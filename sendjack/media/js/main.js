a6d8edce6d2c59d9789abae4bc9a63766d498223
Evan Hammer <hammer@sendjack.com>
2013-02-05 21:29:06 -0500

    [posting-calendar] Add jquery ui.

--- sendjack/media/js/main.js
+++ sendjack/media/js/main.js
@@ -15,12 +15,19 @@ require.config({
         jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min',
         lodash:
             'http://cdnjs.cloudflare.com/ajax/libs/lodash.js/1.0.0-rc.3/lodash.min',
-        backbone: 'libs/backbone/backbone'
+        backbone: 'libs/backbone/backbone',
+
+        jqueryui: 'libs/jquery/jquery-ui'
     },
     shim: {
         'backbone': {
             deps: ['lodash', 'jquery'],
             exports: 'Backbone'
+        },
+
+        'jqueryui': {
+            deps: ['jquery'],
+            exports: 'jqueryui'
         }
     }
 
