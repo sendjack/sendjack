8ae02a1d22b83d6d5c66a72c24185b5085942e2e
Jon Warman <warman@sendjack.com>
2013-02-25 23:45:43 -0500

    [view] add *Field subclasses and instantiate them when creating FieldLists

--- sendjack/view/app/main/components.py
+++ sendjack/view/app/main/components.py
@@ -7,7 +7,8 @@
 """
 from view.elementary.html import Section, HiddenInput
 
-from view.app.base.components import Grid, Title, HeadField, SubField
+from view.app.base.components import Grid, Title
+from view.app.base.components import Field, HeadField, SubField, BigField
 
 
 class NormalSection(Section):
@@ -44,6 +45,64 @@ class TitledGrid(Grid):
         self.append_child(Title(title_str))
 
 
+class IDField(Field):
+
+    LABEL = unicode("ID")
+    KEY = unicode("id")
+    CLASS = unicode("id")
+
+    def __init__(self, value=""):
+        super(IDField, self).__init__(self.LABEL, self.KEY, value)
+        self.append_class(self.CLASS)
+
+
+class CreatorField(Field):
+
+    LABEL = unicode("Creator")
+    KEY = unicode("creator_id")
+    CLASS = unicode("creator-id")
+
+    def __init__(self, value=""):
+        super(CustomerTitleField, self).__init__(self.LABEL, self.KEY, value)
+        self.append_class(self.CLASS)
+
+
+class CustomerTitleField(Field):
+
+    LABEL = unicode("Title")
+    KEY = unicode("customer_title")
+    CLASS = unicode("customer-title")
+
+    def __init__(self, value=""):
+        super(CustomerTitleField, self).__init__(self.LABEL, self.KEY, value)
+        self.append_class(self.CLASS)
+
+
+class CustomerDescriptionField(BigField):
+
+    LABEL = unicode("Steps")
+    KEY = unicode("customer_description")
+    CLASS = unicode("customer-description")
+
+    def __init__(self, value=""):
+        super(CustomerDescriptionField, self).__init__(
+                self.LABEL,
+                self.KEY,
+                value)
+        self.append_class(self.CLASS)
+
+
+class TitleField(Field):
+
+    LABEL = unicode("Title")
+    KEY = unicode("title")
+    CLASS = unicode("title")
+
+    def __init__(self, value=""):
+        super(TitleField, self).__init__(self.LABEL, self.KEY, value)
+        self.append_class(self.TITLE_CLASS)
+
+
 class StepsField(HeadField):
 
     STEPS_LABEL = unicode("Steps")
@@ -53,7 +112,7 @@ class StepsField(HeadField):
     def __init__(self, value=""):
         super(StepsField, self).__init__(self.STEPS_LABEL, self.STEPS_KEY)
         self.append_class(self.STEPS_CLASS)
-        self.append_child(HiddenInput(self.STEPS_KEY))
+        self.append_child(HiddenInput(self.STEPS_KEY, value))
 
 
 class StepField(SubField):
@@ -69,3 +128,91 @@ class StepField(SubField):
                 value,
                 include_index)
         self.append_class(self.STEP_CLASS)
+
+
+class DeadlineField(Field):
+
+    LABEL = unicode("Deadline")
+    KEY = unicode("deadline_ts")
+    CLASS = unicode("deadline-ts")
+
+    def __init__(self, value=""):
+        super(DeadlineField, self).__init__(self.LABEL, self.KEY, value)
+        self.append_class(self.CLASS)
+
+
+class PriceField(Field):
+
+    LABEL = unicode("Price")
+    KEY = unicode("price")
+    CLASS = unicode("price")
+
+    def __init__(self, value=""):
+        super(PriceField, self).__init__(self.LABEL, self.KEY, value)
+        self.append_class(self.CLASS)
+
+
+class OutputTypeField(Field):
+
+    LABEL = unicode("Output Type")
+    KEY = unicode("output_type")
+    CLASS = unicode("output-type")
+
+    def __init__(self, value=""):
+        super(OutputTypeField, self).__init__(self.LABEL, self.KEY, value)
+        self.append_class(self.CLASS)
+
+
+class OutputMethodField(Field):
+
+    LABEL = unicode("Output Method")
+    KEY = unicode("output_method")
+    CLASS = unicode("output-method")
+
+    def __init__(self, value=""):
+        super(OutputMethodField, self).__init__(self.LABEL, self.KEY, value)
+        self.append_class(self.CLASS)
+
+
+class CategoryTagsField(Field):
+
+    LABEL = unicode("Category Tags")
+    KEY = unicode("category_tags")
+    CLASS = unicode("category-tags")
+
+    def __init__(self, value=""):
+        super(CategoryTagsField, self).__init__(self.LABEL, self.KEY, value)
+        self.append_class(self.CLASS)
+
+
+class IndustryTagsField(Field):
+
+    LABEL = unicode("Industry Tags")
+    KEY = unicode("industry_tags")
+    CLASS = unicode("industry-tags")
+
+    def __init__(self, value=""):
+        super(IndustryTagsField, self).__init__(self.LABEL, self.KEY, value)
+        self.append_class(self.CLASS)
+
+
+class SkillsTagsField(Field):
+
+    LABEL = unicode("Skills Tags")
+    KEY = unicode("skills_tags")
+    CLASS = unicode("skills-tags")
+
+    def __init__(self, value=""):
+        super(SkillsTagsField, self).__init__(self.LABEL, self.KEY, value)
+        self.append_class(self.CLASS)
+
+
+class EquipmentTagsField(Field):
+
+    LABEL = unicode("Equipment Tags")
+    KEY = unicode("equipment_tags")
+    CLASS = unicode("equipment-tags")
+
+    def __init__(self, value=""):
+        super(EquipmentTagsField, self).__init__(self.LABEL, self.KEY, value)
+        self.append_class(self.CLASS)
