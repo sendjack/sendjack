32637105f89ea8797c6e77cd9199f35bf4a948e2
Jon Warman <warman@sendjack.com>
2013-04-20 02:26:21 -0400

    [temporary] add some print lines to figure out what the fuck is happening

--- sendjack/handlers/sync/base.py
+++ sendjack/handlers/sync/base.py
@@ -88,14 +88,16 @@ class SecureSyncHandler(SyncHandler):
         to prove we are functionally secure. When we have an SSL certificate
         for a wildcard subdomain, we can drop the GET argument hack."""
         # build an absolute url to redirect internally to https.
+        print "secure_redirect"
         url = SecureEmbeddableURL(self.request.path, self.request.query)
-
+        print "SecureEmbeddableURL: ", url.render()
         # permanent=True also implicitly means status=301.
         self.redirect(url.render(), True)
 
 
     def _is_request_secure(self):
         """Is the current request is secure (using https and ssl)?"""
+        print "_is_request_secure"
         url = URL()
 
         # xheaders=True already ensures that self.request.protocol is set to
@@ -108,4 +110,7 @@ class SecureSyncHandler(SyncHandler):
         # staging, so hack the query string to fake ssl for now.
         url.set_query(self.request.query)
 
+        print "url.protocol: ", url.protocol
+        print "url.query: ", url.query
+        print "url.is_secure: ", url.is_secure()
         return url.is_secure()
